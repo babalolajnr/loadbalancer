dbdb3a6901d429c023ea832942428da6b1e43b2f
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-08-23 09:17:21 +0100

    Create ResponseController class

---
+++ src/com/loadbalancer/ResponseController.java
@@ -0,0 +1,29 @@
+package com.loadbalancer;
+
+public class ResponseController implements ResponseMatcher {
+    private String request;
+    private String matchKey;
+    private String response;
+
+    public ResponseController(String message) {
+        this.request = message;
+        this.matchKey = getKey(message);
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public String getKey() {
+        return matchKey;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public String getKey(String request) {
+        //set the matcher key
+        return request.substring(0, 13);
+    }
+}
