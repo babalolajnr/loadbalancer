2685c7556ae8073018364db5c81231f6d7001f29
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-08-23 09:16:45 +0100

    Create NodePool class

---
+++ src/com/loadbalancer/NodePool.java
@@ -0,0 +1,17 @@
+package com.loadbalancer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NodePool {
+    private List<NodesManager> nodeList;
+
+    public List<NodesManager> getNodeList() {
+        return nodeList;
+    }
+
+    public void setNodeList(List<NodesManager> nodeList) {
+        this.nodeList = nodeList;
+    }
+
+}
