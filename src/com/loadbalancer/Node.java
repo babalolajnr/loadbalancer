8365409dc068b1618fbd8385ed67492abcdfcfd6
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-08-25 19:59:01 +0100

    refactor class

--- src/com/loadbalancer/Node.java
+++ src/com/loadbalancer/Node.java
@@ -1,37 +1,20 @@
 package com.loadbalancer;
 
 public class Node {
-    private String ip;
-    private String name;
-    private int port;
+    private final String ip;
+    private final int port;
 
-    public Node(String ip, String name, int port) {
+    public Node(String ip, int port) {
         this.ip = ip;
-        this.name = name;
         this.port = port;
-    }
-
-    public String getNodeName() {
-        return this.name;
-    }
-
-    public void setNodeName(String nodeName) {
-        this.name = nodeName;
     }
 
     public String getIp() {
         return this.ip;
     }
 
-    public void setIp(String ip) {
-        this.ip = ip;
-    }
-
     public int getPort() {
         return this.port;
     }
 
-    public void setPort(int port) {
-        this.port = port;
-    }
 }
